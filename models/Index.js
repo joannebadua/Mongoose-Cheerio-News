ee1b29ada96caa802310380b578a94718bd169fd
Joanne Badua <joannebadua@gmail.com>
2019-07-29 15:08:40 -0700

    changed name of models index.

--- models/Index.js
+++
@@ -1,8 +0,0 @@
-// Exporting an object containing all of our models
-
-module.exports = {
-    Lawmakers: require("./Lawmakers"),
-    Comment: require("./Comment")
-  };
-  
-//allows to have multiple schema and import them at the same time
